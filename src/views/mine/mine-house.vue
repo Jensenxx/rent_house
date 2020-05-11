ac956c7f4f18b1f5938c1f29e494f2846e4bccd5
jensen <979399019@qq.com>
2020-05-11 22:12:56 +0800

    fix: mine-house publish

--- src/views/mine/mine-house.vue
+++ src/views/mine/mine-house.vue
@@ -19,7 +19,7 @@
             <p>户型: {{item.unitType}}</p>
             <p>地址: {{`${item.province}${item.city}${item.region}${item.descAddress}${item.building}栋${item.unit}单元`}}</p>
             <p>租金: {{item.rent}}/月</p>
-            <van-button size="small" type="info" class="publish" :disabled="item.flag" @click.stop="publishHourse(item)">发布</van-button>
+            <van-button size="small" type="info" class="publish" :disabled="item.flag || item.status === 'RENTED'" @click.stop="publishHourse(item)">发布</van-button>
             <van-button size="small" type="danger" :disabled="item.status === 'RENTED'" @click.stop="handleDel(item)">删除</van-button>
           </div>
         </div>
